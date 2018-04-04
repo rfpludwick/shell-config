61d04f09b9ed460fe88943257a584c9f5bade0aa
Robert F.P. Ludwick <robertl@cdbaby.com>
2018-04-04 10:36:16 -0700

    pushd/popd for git-less installation

--- install-no-git.sh
+++ install-no-git.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir $HOME/.bash-config
-cd $HOME/.bash-config
+pushd $HOME/.bash-config
 wget https://github.com/rfpludwick/bash-config/archive/master.tar.gz
 tar -xvf master.tar.gz --strip-components=1
 rm master.tar.gz
 ./install.sh
+popd
