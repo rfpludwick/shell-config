6cddb75989aaa607372a71f799969bd4099381d8
Robert F.P. Ludwick <robert.ludwick@workday.com>
2022-02-17 08:31:23 -0800

    Configs update

--- bash/install/main.sh
+++ bash/install/main.sh
@@ -27,44 +27,76 @@ if [ "$EUID" -ne 0 ] || grep --quiet docker /proc/1/cgroup || [ -f /.dockerenv ]
 	fi
 fi
 
+# Create config directory for Starship, if necessary
 if [ ! -d "${HOME}"/.config ]; then
+	echo Creating user config directory
+
 	mkdir -v "${HOME}"/.config
 fi
 
 echo Archiving old Bash configuration...
 
 # Check if we're archiving an old installation directory first
-if [ -d "${HOME}/.bash-config" ]; then
-	mv -v "${HOME}/.bash-config" "${HOME}/.shell-config-archive"
+if [ -d "${HOME}"/.bash-config-archive ]; then
+	echo Removing old shell config archive directory
+
+	rm -Rfv "${HOME}"/.shell-config-archive
+fi
+
+if [ -d "${HOME}"/.bash-config ]; then
+	echo Archiving existing bash config directory
+
+	mv -v "${HOME}"/.bash-config "${HOME}"/.shell-config-archive
+elif [ -d "${HOME}"/.shell-config ] && [ ! -L "${HOME}"/.shell-config ]; then
+	echo Archiving existing shell config directory
+
+	mv -v "${HOME}"/.bash-config "${HOME}"/.shell-config-archive
 elif [ ! -d "${HOME}"/.shell-config-archive ]; then
+	echo Creating shell config archive directory
+
 	mkdir -v "${HOME}"/.shell-config-archive
 fi
 
+# Archive files
 if [ -f "${HOME}"/.bash_aliases ] && [ ! -L "${HOME}"/.bash_aliases ]; then
+	echo Archiving existing bash aliases
+
 	mv -v "${HOME}"/.bash_aliases "${HOME}"/.shell-config-archive/bash_aliases
 fi
 
 if [ -f "${HOME}"/.bash_exports ] && [ ! -L "${HOME}"/.bash_exports ]; then
+	echo Archiving existing bash exports
+
 	mv -v "${HOME}"/.bash_exports "${HOME}"/.shell-config-archive/bash_exports
 fi
 
 if [ -f "${HOME}"/.bash_logout ] && [ ! -L "${HOME}"/.bash_logout ]; then
+	echo Archiving existing bash logout
+
 	mv -v "${HOME}"/.bash_logout "${HOME}"/.shell-config-archive/bash_logout
 fi
 
 if [ -f "${HOME}"/.bash_profile ] && [ ! -L "${HOME}"/.bash_profile ]; then
+	echo Archiving existing bash profile
+
 	mv -v "${HOME}"/.bash_profile "${HOME}"/.shell-config-archive/bash_profile
 fi
 
 if [ -f "${HOME}"/.bashrc ] && [ ! -L "${HOME}"/.bashrc ]; then
+	echo Archiving existing bashrc
+
 	mv -v "${HOME}"/.bashrc "${HOME}"/.shell-config-archive/bashrc
 fi
 
 if [ -f "${HOME}"/.config/starship.toml ] && [ ! -L "${HOME}"/.config/starship.toml ]; then
+	echo Archiving existing Starship configuration
+
 	mv -v "${HOME}"/.config/starship.toml "${HOME}"/.shell-config-archive/starship.toml
 fi
 
 if [ -f "${HOME}"/.vimrc ] && [ ! -L "${HOME}"/.vimrc ]; then
+	echo Archiving existing vimrc
+
 	mv -v "${HOME}"/.vimrc "${HOME}"/.shell-config-archive/vimrc
 fi
 
