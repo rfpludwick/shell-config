f0d09ba40e21d5251c7dcf42bd69e48693c3ba3e
Robert F.P. Ludwick <robert@ludwick.us>
2024-02-01 14:56:47 -0800

    No longer support installing for root

--- bash/install/no-git.sh
+++ bash/install/no-git.sh
@@ -2,21 +2,6 @@
 
 set -exo pipefail
 
-SUDO_STARSHIP_INSTALL=""
-
-while getopts "s" options; do
-	case "${options}" in
-		s)
-			SUDO_STARSHIP_INSTALL="-s"
-			;;
-		*)
-			echo Unsupported getopts
-			exit 1
-			;;
-	esac
-done
-shift $((OPTIND-1))
-
 mkdir --parents "${HOME}"/.shell-config
 pushd "${HOME}"/.shell-config
 curl --location https://codeload.github.com/rfpludwick/shell-config/tar.gz/main --output shell-config.tgz
@@ -25,4 +10,4 @@ rm shell-config.tgz
 popd
 
 # shellcheck disable=SC1091
-"${HOME}"/.shell-config/bash/install/main.sh "${SUDO_STARSHIP_INSTALL}"
+"${HOME}"/.shell-config/bash/install/main.sh
